65b614f0b0a102de92d3b26e018365e8abda681a
gmh5225 <2315157@qq.com>
2024-10-29 07:57:44 +0800

    upload InscriptionProxyFactory.sol

---
+++ src/InscriptionProxyFactory.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract InscriptionProxyFactory is Ownable {
+    // custom errors
+    error NotOwner();
+
+    ProxyAdmin public immutable proxyAdmin;
+
+    constructor() Ownable(msg.sender) {
+        proxyAdmin = new ProxyAdmin(msg.sender);
+    }
+
+    // deploy proxy
+    function deployProxy(address logic, bytes memory data) external onlyOwner returns (address) {
+        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(logic, address(proxyAdmin), data);
+        return address(proxy);
+    }
+
+    // upgrade proxy
+    function upgradeProxy(address proxy, address newImplementation, bytes memory data) external onlyOwner {
+        proxyAdmin.upgradeAndCall(ITransparentUpgradeableProxy(proxy), newImplementation, data);
+    }
+}
