c6ed5e21833cc9868045299eb76f28786e71c006
gmh5225 <2315157@qq.com>
2024-10-29 21:37:51 +0800

    update

--- script/UpgradeToV2.s.sol
+++ script/UpgradeToV2.s.sol
@@ -10,6 +10,7 @@ import "../src/InscriptionLogicV2.sol";
 contract UpgradeToV2Script is Script {
     address constant V2_ADDRESS = address(0x5c77Ff2668b4defb7F17B74b9C3d05026E160F78);
     address constant PROXY_ADDRESS = address(0xc7B704D8D43e554518ed324fB85Cd7067B56591d);
+    //address constant TOKEN_V2_ADDRESS = address(0xFeBcDAd45083e5b3eBf30e0F974FC081bcd76CF9);
 
     function run() external {
         // TODO: encrypt your private key
@@ -37,5 +38,18 @@ contract UpgradeToV2Script is Script {
 
         // stop broadcast
         vm.stopBroadcast();
+
+        /*
+            Deployed InscriptionLogicV2 token address: 0xFeBcDAd45083e5b3eBf30e0F974FC081bcd76CF9
+            Implementation contract: 0x267dcfe927125553108D305a4b398041115bd8Da
+            Token info.totalSupply: 1000
+            Token info.perMint: 100
+            Token info.mintedAmount: 0
+            Token info.price: 10000000000000000
+         */
+
+        // InscriptionToken inscriptionToken = InscriptionToken(TOKEN_V2_ADDRESS);
+        // // InscriptionToken symbol: ISLC2
+        // console2.log("InscriptionToken symbol:", inscriptionToken.symbol());
     }
 }
