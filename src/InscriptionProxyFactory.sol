ce77d4046533d98a304e9bbcb30ac22ea34ec1c2
gmh5225 <2315157@qq.com>
2024-10-29 08:04:48 +0800

    update

--- src/InscriptionProxyFactory.sol
+++ src/InscriptionProxyFactory.sol
@@ -6,20 +6,34 @@ import "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract InscriptionProxyFactory is Ownable {
+    // custom errors
+    error ProxyDeployFailed();
+    error UpgradeFailed();
+
+    // events
+    event ProxyDeployed(address indexed proxy, address indexed implementation);
+    event ProxyUpgraded(address indexed proxy, address indexed implementation);
+
     ProxyAdmin public immutable proxyAdmin;
 
     constructor() Ownable(msg.sender) {
-        proxyAdmin = new ProxyAdmin(owner());
+        proxyAdmin = new ProxyAdmin(msg.sender);
     }
 
     // deploy proxy
-    function deployProxy(address logic, bytes memory data) external onlyOwner returns (address) {
-        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(logic, address(proxyAdmin), data);
-        return address(proxy);
+    function deployProxy(address implementation, bytes calldata initData) external onlyOwner returns (address proxy) {
+        proxy = address(new TransparentUpgradeableProxy(implementation, address(proxyAdmin), initData));
+        if (proxy == address(0)) revert ProxyDeployFailed();
+
+        emit ProxyDeployed(proxy, implementation);
     }
 
     // upgrade proxy
-    function upgradeProxy(address proxy, address newImplementation, bytes memory data) external onlyOwner {
-        proxyAdmin.upgradeAndCall(ITransparentUpgradeableProxy(proxy), newImplementation, data);
+    function upgradeProxy(address proxy, address newImplementation, bytes calldata data) external onlyOwner {
+        try proxyAdmin.upgradeAndCall(ITransparentUpgradeableProxy(proxy), newImplementation, data) {
+            emit ProxyUpgraded(proxy, newImplementation);
+        } catch {
+            revert UpgradeFailed();
+        }
     }
 }
