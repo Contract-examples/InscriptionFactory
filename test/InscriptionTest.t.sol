1dbfcee23876267bac7236dd596641985c40198e
gmh5225 <2315157@qq.com>
2024-11-07 14:51:41 +0800

    update

--- test/InscriptionTest.t.sol
+++ test/InscriptionTest.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.28;
 
 import "forge-std/Test.sol";
 import "forge-std/console2.sol";
+import "@openzeppelin-upgradeable/contracts/utils/PausableUpgradeable.sol";
 import "@uups-proxy-factory-sdk/sdk/IUUPSProxyFactory.sol";
 import "../src/InscriptionLogic.sol";
 import "../src/InscriptionLogicV2.sol";
@@ -116,6 +117,43 @@ contract InscriptionTest is Test {
         assertEq(inscriptionToken.balanceOf(user1), 100);
         assertEq(address(proxy).balance, 10 ether);
         vm.stopPrank();
+    }
+
+    function test_MintInscriptionV2WhenPaused() public {
+        vm.startPrank(owner);
+
+        logicV2 = new InscriptionLogicV2();
+        InscriptionLogic(proxy).upgradeToAndCall(
+            address(logicV2), abi.encodeWithSelector(InscriptionLogicV2.initialize.selector, owner)
+        );
+
+        InscriptionLogicV2 logicV2Instance = InscriptionLogicV2(proxy);
+        address token = logicV2Instance.deployInscription("test2", 1000, 100, 0.1 ether);
+
+        console2.log("Deployed token address:", token);
+        console2.log("Implementation contract:", logicV2Instance.implementationContract());
+
+        InscriptionLogicV2.TokenInfo memory info = logicV2Instance.getTokenInfo(token);
+
+        console2.log("Token info.totalSupply:", info.totalSupply);
+        console2.log("Token info.perMint:", info.perMint);
+        console2.log("Token info.mintedAmount:", info.mintedAmount);
+        console2.log("Token info.price:", info.price);
+
+        vm.stopPrank();
+
+        // pause the contract
+        vm.startPrank(owner);
+        logicV2Instance.pause();
+        vm.stopPrank();
+
+        vm.deal(user1, 10 ether);
+        vm.startPrank(user1);
+        vm.expectRevert(0xd93c0665);
+        logicV2Instance.mintInscription{ value: 10 ether }(token);
+
+        console2.log("User1 balance:", user1.balance);
+        console2.log("Proxy balance:", address(proxy).balance);
     }
 
     function test_WithdrawFeesV2() public {
